64da8cab9316b9518a19ef5fdaebaf885635e4a9
bugbiteme <leon.s.levy@gmail.com>
2016-05-22 23:11:24 -0700

    added get all VNX from customers

--- main.py
+++ main.py
@@ -46,7 +46,33 @@ def getInstallBaseCust():
 
 		if con:
 			con.close()
+
+def getInstallBaseVNX():
+	try:
+		con = lite.connect('installbase.db')
+	
+		cur = con.cursor()
+		cur.execute('select CS_CUSTOMER_NAME, PRODUCT_GROUP, ITEM_SERIAL_NUMBER from tbl_installbase where PRODUCT_GROUP like \'VNX%\'')
+	
+		rows = cur.fetchall()
+	
+		print "Here are all of your customers:"
+		for row in rows:
+			print row[0]
+			print row[1]
+			print row[2]
+	
+	except lite.Error, e:
+
+		print "Error %s:" % e.args[0]
+		sys.exit(1)
+	
+	finally:
+
+		if con:
+			con.close()
 						
 
 getSQLiteVersion()
 getInstallBaseCust()
+getInstallBaseVNX()
