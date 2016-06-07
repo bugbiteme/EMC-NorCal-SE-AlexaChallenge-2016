7d65df0b18c010eab2926c7ce6b36de64b1044ef
Leon Levy <leon.s.levy@gmail.com>
2016-06-07 08:52:10 -0700

    timestamp in get function

--- webappform/bin/app.py
+++ webappform/bin/app.py
@@ -1,7 +1,7 @@
 import web
 import json
 import sql_wrapper as sql
-
+from datetime import datetime
 
 
 urls = ('/ibResponse.json', 'Index')
@@ -13,17 +13,22 @@ class Index(object):
     def GET(self):
         form = web.input(name="Nobody")
         web.header('Content-Type', 'application/json')
+
+        
+        i = datetime.now()
+        
+        print "[" + i.strftime('%d/%m/%Y %H:%M:%S') + "] - - Requesting: " + form.name
         
         if form.name == "version":
             greeting = sql.getSQLiteVersion()
-        elif form.name == "ib_sites":
+        elif form.name == "sites":
             return json.dumps(sql.getInstallBaseCust())
-        elif form.name == "ib_vnx":
+        elif form.name == "vnx":
         	return json.dumps(sql.getInstallBaseVNX())
         elif form.name == "isilon":
         	return json.dumps(sql.getInstallBaseISILON())
         else:
-            return json.dumps({"text": "invalid parameter"})
+            return json.dumps({"text": "nobody has a " + form.name })
 
 if __name__ == "__main__":
     app.run()
